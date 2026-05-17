298230183d003276b36672eff5bae5e93078551e
parthp-dental <parth5thrap@gmail.com>
2026-05-17 20:45:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXgzrGv8van-F99D8HNTKMYqE0gjgblw",
+  apiKey: "AIzaSyAXgzrGvg8van-F99D8HNTKMYqE0gjgblw",
   authDomain: "my-fitness-hub-b12ce.firebaseapp.com",
   projectId: "my-fitness-hub-b12ce",
   storageBucket: "my-fitness-hub-b12ce.firebasestorage.app",
   messagingSenderId: "518738323828",
   appId: "1:518738323828:web:5983222c6d67259aa1e421"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
